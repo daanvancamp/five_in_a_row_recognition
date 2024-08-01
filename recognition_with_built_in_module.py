11c8c3f23f7b9bb429cf2d58e9d79b9293245988
daanvancamp <163452511+daanvancamp@users.noreply.github.com>
2024-08-01 13:38:38 +0200

    added some other boards to test the recognition

--- recognition_with_built_in_module.py
+++ recognition_with_built_in_module.py
@@ -1,1 +1,27 @@
+# import required libraries
+import cv2
 
+number_of_corners=(7,7)
+# read input image
+img = cv2.imread(r'./testopstellingen/bord8.jpg')
+
+# convert the input image to a grayscale
+try:
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+except:
+    raise Exception("check the image path")
+
+# Find the chess board corners
+ret, corners = cv2.findChessboardCorners(gray, number_of_corners,None)
+
+# if chessboard corners are detected
+if ret == True:
+   
+   # Draw and display the corners
+
+   img = cv2.drawChessboardCorners(img, number_of_corners, corners,ret)
+   cv2.imshow('Chessboard',img)
+   cv2.waitKey(0)
+else:
+    print("No chessboard detected")
+cv2.destroyAllWindows()
