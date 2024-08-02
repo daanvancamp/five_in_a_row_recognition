b4a12c24122520d702504fe810f7e6cc9e877f68
daanvancamp <163452511+daanvancamp@users.noreply.github.com>
2024-08-02 15:39:22 +0200

    added vars where necessary, so the board size can easily be changed

--- recognition_with_built_in_module.py
+++ recognition_with_built_in_module.py
@@ -4,18 +4,14 @@ import cv2
 from math import sqrt
 import numpy as np
 BOARD_SIZE=8
+corners_to_be_found=7
 
 def draw_point_and_show(image,point):
-    point = (368, 365)  # Hier gebruiken we gehele getallen voor de pixelco�rdinaten
-
-    # Kleur van het punt in BGR (blauw, groen, rood)
     color = (0, 0, 255)  # Rood in BGR
 
-    # Grootte van de cirkel (straal) en dikte van de rand
     radius = 10
-    thickness = -1  # Vul de cirkel in (negatieve waarde betekent vullen)
+    thickness = -1  
 
-    # Tekenen van het punt
     cv2.circle(image, point, radius, color, thickness)
     cv2.namedWindow("Corners", cv2.WINDOW_NORMAL)
 
@@ -78,15 +74,14 @@ def extrapolate_other_corners(list_corners_tuples,corners,average_horizontal_dis
 
 
 def get_other_corners(corners,image):
-    global BOARD_SIZE
+    global BOARD_SIZE,corners_to_be_found
     #order: from top left to bottom right, row by row
     corners = corners[corners[:, 0, 0].argsort()] #sort per column
     
     tuple_kolom=()
     list_tuples_corners=[]
-
-    for i in range(BOARD_SIZE):
-        for i in range(BOARD_SIZE): #order: from top left to bottom right, row by row)
+    for i in range(corners_to_be_found):
+        for i in range(corners_to_be_found): #order: from top left to bottom right, row by row)
             tuple_kolom+=((corners[i][0][0],corners[i][0][1]),)
         list_tuples_corners.append(tuple_kolom)
     #print(list_tuples_corners)
@@ -103,7 +98,7 @@ def get_other_corners(corners,image):
 
     return list_tuples_corners
 
-number_of_corners=(7,7)
+number_of_corners=(corners_to_be_found,corners_to_be_found)
 # read input image
 img = cv2.imread(r'./testopstellingen/bord8.jpg')
 
@@ -119,8 +114,6 @@ ret, corners = cv2.findChessboardCorners(gray, number_of_corners,None)#optional 
 # if chessboard corners are detected
 if ret == True:
     print("Chessboard detected")
-
- 
     corners=get_other_corners(corners,img)
     print("corners:",corners)
     corners=np.unique(corners,axis=0)
