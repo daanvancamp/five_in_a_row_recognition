57d8797023eef643508f3b27e9b978af833d1464
daanvancamp <163452511+daanvancamp@users.noreply.github.com>
2024-08-21 12:55:19 +0200

    small update

--- recognition_version to built-in.py
+++ recognition_version to built-in.py
@@ -190,14 +190,6 @@ def match_shapes_to_centers(shapes, cell_centers, img,color):
 
     return list_shapes
 
-def crop_to_square(frame):
-    height, width = frame.shape[:2]
-    smallest_side = min(height, width)
-    start_x = (width - smallest_side) // 2 
-    start_y = (height - smallest_side) // 2
-    square_frame = frame[start_y:start_y + smallest_side, start_x:start_x + smallest_side]
-    return square_frame
-
 def main():
     number_of_corners = (corners_to_be_found, corners_to_be_found)
     cap=cv2.VideoCapture(1)
